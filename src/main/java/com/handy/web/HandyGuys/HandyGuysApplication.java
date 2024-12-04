423e5940ada49237447cc2fbbcc12763c344b9aa
Rancy21 <rancyontsandaga@gmail.com>
2024-12-04 15:27:27 +0200

    Initial Commit

---
+++ src/main/java/com/handy/web/HandyGuys/HandyGuysApplication.java
@@ -0,0 +1,13 @@
+package com.handy.web.HandyGuys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyGuysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyGuysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handy/web/HandyGuys/HandyGuysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handy.web.HandyGuys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyGuysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
