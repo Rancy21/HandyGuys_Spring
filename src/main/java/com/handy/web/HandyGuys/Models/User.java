2f5ef8a5e6fb4f9e6ab5083d17460dd2e58ccf2f
Paola-Diele <dielemamfoumbi@gmail.com>
2024-12-04 15:44:16 +0200

    i created a new model class

---
+++ src/main/java/com/handy/web/HandyGuys/Models/User.java
@@ -0,0 +1,12 @@
+package com.handy.web.HandyGuys.Models;
+
+import java.util.UUID;
+
+public class User {
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String password;
+}
