5ebc46d7f0a10e204bc19d3b91417d9eed111f49
Paola-Diele <dielemamfoumbi@gmail.com>
2024-12-05 14:36:13 +0200

    added save method to sign up tracker service

--- src/main/java/com/handy/web/HandyGuys/service/SignUpTrackerService.java
+++ src/main/java/com/handy/web/HandyGuys/service/SignUpTrackerService.java
@@ -1,9 +1,19 @@
 package com.handy.web.HandyGuys.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.handy.web.HandyGuys.Models.SignUpTracker;
+import com.handy.web.HandyGuys.repository.ISignUpTrackerRepository;
 
 @Service
 public class SignUpTrackerService {
     
-    
+    @Autowired
+    ISignUpTrackerRepository repo;
+
+    public String saveTracker(SignUpTracker signUT){
+        repo.save(signUT);
+        return "SignUp Tracker saved successfully";
+    }
 }
