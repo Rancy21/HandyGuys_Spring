ac4c5e9f832d5ae21dfbf131549b80ede9ee445d
Paola-Diele <dielemamfoumbi@gmail.com>
2024-12-06 20:20:05 +0200

    create weconfig

---
+++ src/main/java/com/handy/web/HandyGuys/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.handy.web.HandyGuys.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all endpoints
+                        .allowedOrigins("http://localhost:5173") // Allow React app's origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // Allow specific HTTP methods
+            }
+        };
+    }
+}
