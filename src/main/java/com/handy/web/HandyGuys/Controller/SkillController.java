8ea1cfb9faf1de4a8079a10a745895a68def2f0c
Rancy21 <rancyontsandaga@gmail.com>
2024-12-07 14:24:35 +0200

    Added get categories

--- src/main/java/com/handy/web/HandyGuys/Controller/SkillController.java
+++ src/main/java/com/handy/web/HandyGuys/Controller/SkillController.java
@@ -1,15 +1,20 @@
 package com.handy.web.HandyGuys.Controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.handy.web.HandyGuys.Models.ECategory;
 import com.handy.web.HandyGuys.Models.Skill;
 import com.handy.web.HandyGuys.Models.User;
 import com.handy.web.HandyGuys.service.SkillService;
@@ -74,6 +79,15 @@ public class SkillController {
         } else {
             return new ResponseEntity<>("Skill successfully deleted", HttpStatus.OK);
         }
+    }
+
+    @GetMapping(value = "/getCategories", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getCategories() {
+        List<String> categories = Arrays.stream(ECategory.values())
+                .map(ECategory::getDisplayName)
+                .toList();
+
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
 }
