ad0b44a8b9e55c84d5841d8f796a7af23c84dd01
Rancy21 <rancyontsandaga@gmail.com>
2024-12-07 14:24:13 +0200

    Added display name in the category enum

--- src/main/java/com/handy/web/HandyGuys/Models/ECategory.java
+++ src/main/java/com/handy/web/HandyGuys/Models/ECategory.java
@@ -1,5 +1,17 @@
 package com.handy.web.HandyGuys.Models;
 
 public enum ECategory {
-    Plumbing, Cleaning
+    Electrical_repair("Electrical Repair"),
+    Plumbing("Plumbing"),
+    Cleaning("Cleaning");
+
+    private final String displayName;
+
+    ECategory(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
